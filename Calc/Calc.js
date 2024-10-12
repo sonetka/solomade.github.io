9d08e03fd5bc0e8b113e7de8e61d17727eb5e67f
sonetka <152024254+sonetka@users.noreply.github.com>
2024-10-12 22:39:47 +0300

    Update Calc.js

--- Calc/Calc.js
+++ Calc/Calc.js
@@ -1,19 +1,21 @@
-window.addEventListener("DOMContentLoaded", function () 
-{
-  const IphoneCost = {"v1": 60000, "v2": 80000, "v3": 100000};
-  let Field = document.getElementById("field");
-  let Model = document.getElementById("model");
-  let CalcButton = document.getElementById("buttonCalc");
-  let Result = document.getElementById("result");
-  CalcButton.addEventListener("click", function () {
-        let input = Field.value;
-        if (input.match(/^\d+$/) !== null) {
-            let x = IphoneCost[Model.value] * input;
-            Result.innerText = x;
+window.addEventListener("DOMContentLoaded", function () {
+    const IphoneCost =
+    {"v1": 60000, "v2": 80000, "v3": 100000};
+
+    let Field = document.getElementById("field");
+    let model = document.getElementById("model");
+    let CalcButton = document.getElementById("buttonCalc");
+    let Result = document.getElementById("result");
+    CalcButton.addEventListener("click", function () {
+        let inp = Field.value;
+        if (inp.match(/^\d+$/) !== null) 
+        {
+            let res = IphoneCost[model.value] * inp;
+            Result.innerText = res;
         } 
         else 
         {
-            Result.innerText = "Некорректный ввод";
+            Result.innerText = "Некорректный ввод количества";
         }
     });
 });
