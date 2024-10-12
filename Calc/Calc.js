805e91bd725f7fd831b9e65c690efa48a720fb4c
sonetka <152024254+sonetka@users.noreply.github.com>
2024-10-12 21:56:27 +0300

    Update Calc.js

--- Calc/Calc.js
+++ Calc/Calc.js
@@ -1,16 +1,18 @@
 window.addEventListener("DOMContentLoaded", function () 
 {
   const IphoneCost = {"v1": 60000, "v2": 80000, "v3": 100000};
-  let field = document.getElementById("field");
-  let model = document.getElementById("model");
+  let Field = document.getElementById("field");
+  let Model = document.getElementById("model");
   let CalcButton = document.getElementById("buttonCalc");
   let Result = document.getElementById("result");
   CalcButton.addEventListener("click", function () {
-        let input = field.value;
+        let input = Field.value;
         if (input.match(/^\d+$/) !== null) {
-            let x = IphoneCost[model.value] * input;
+            let x = IphoneCost[Model.value] * input;
             Result.innerText = x;
-        } else {
+        } 
+        else 
+        {
             Result.innerText = "Некорректный ввод";
         }
     });
