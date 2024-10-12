eca059e8d506f086490c8c2f190540c189dbc8ad
sonetka <152024254+sonetka@users.noreply.github.com>
2024-10-12 18:48:37 +0300

    Create Calc.js

---
+++ Calc/Calc.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", function () 
+{
+  const IphoneCost = {"v1": 60000, "v2": 80000, "v3": 100000};
+  let field = document.getElementById("field");
+  let model = document.getElementById("model");
+  let CalcButton = document.getElementById("buttonCalc");
+  let Result = document.getElementById("result");
+  CalcButton.addEventListener("click", function () {
+        let input = field.value;
+        if (inp.match(/^\d+$/) !== null) {
+            let x = IphoneCost[model.value] * inp;
+            Result.innerText = x;
+        } else {
+            Result.innerText = "Некорректный ввод";
+        }
+    });
+}
