8bd3daf419f3977a8369db4db0653dd6969b8c96
sonetka <152024254+sonetka@users.noreply.github.com>
2024-10-14 12:27:00 +0300

    Update calc2.js

--- Calc2/calc2.js
+++ Calc2/calc2.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const IphoneCost =
     {"v1": 60000, "v2": 80000, "v3": 100000};
+    const IphoneComplectation =
+    {"c1": 0, "c2": 3000, "c3": 15000};
+    const IphoneServises =
+    {"s1": 300, "s2": 1500};
+
 
     let field = document.getElementById("field");
     let model = document.getElementById("model");
     let buttonCalc = document.getElementById("buttonCalc");
     let result = document.getElementById("result");
+    let radio = document.getElementByName("Iphone");
+    let servises = document.getElementByName("servises");
     buttonCalc.addEventListener("click", function () {
         let inp = field.value;
         if (inp.match(/^\d+$/) !== null) 
